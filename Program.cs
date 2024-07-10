a8ed6e8ac84dd65da5c400780af00520083b0673
Vinicius Almeida <vinicius.almeida2011@gmail.com>
2024-07-10 14:31:51 -0300

    Criado classe posicao

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tabuleiro;
+
+namespace ProjetoXadrezConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P = new Posicao(3,4);
+            Console.WriteLine("Posição: " + P);
+            Console.ReadLine();
+        }
+    }
+}
