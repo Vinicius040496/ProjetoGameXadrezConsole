b9cbe0989d73b05b2a1bbea8f94ea9dfcbbc594e
Vinicius Almeida <vinicius.almeida2011@gmail.com>
2024-07-11 15:30:19 -0300

    Tratando erros com exceptions

--- Program.cs
+++ Program.cs
@@ -12,11 +12,18 @@ namespace ProjetoXadrezConsole
     {
         static void Main(string[] args)
         {
-           Tabuleiro tab = new Tabuleiro(8,8);
-            tab.ColocarPeca(new Torre(tab,Cor.Preta), new Posicao(0, 0));
-            tab.ColocarPeca(new Torre(tab,Cor.Preta), new Posicao(1, 3));
-            tab.ColocarPeca(new Rei(tab,Cor.Preta), new Posicao(2, 4));
-            Tela.ImprimirTabuleiro(tab);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                Tela.ImprimirTabuleiro(tab);
+            }
+            catch (TabuleiroExeptions ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             Console.ReadLine();
         }
     }
