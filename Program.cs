88a8be6750f177d739840074d96a928f7060c1f8
Vinicius Almeida <vinicius.almeida2011@gmail.com>
2024-07-11 15:09:52 -0300

    comecando a colocar pecas no tabuleiro

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using tabuleiro;
+using Xadrez;
 
 namespace ProjetoXadrezConsole
 {
@@ -12,6 +13,9 @@ namespace ProjetoXadrezConsole
         static void Main(string[] args)
         {
            Tabuleiro tab = new Tabuleiro(8,8);
+            tab.ColocarPeca(new Torre(tab,Cor.Preta), new Posicao(0, 0));
+            tab.ColocarPeca(new Torre(tab,Cor.Preta), new Posicao(1, 3));
+            tab.ColocarPeca(new Rei(tab,Cor.Preta), new Posicao(2, 4));
             Tela.ImprimirTabuleiro(tab);
             Console.ReadLine();
         }
