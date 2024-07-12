7a90650614565fa2fc1a0c93a6dd3943204eaa70
Vinicius Almeida <vinicius.almeida2011@gmail.com>
2024-07-12 17:57:18 -0300

    Testando movimentacoes do tabuleiro

--- Program.cs
+++ Program.cs
@@ -14,13 +14,22 @@ namespace ProjetoXadrezConsole
         {
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                PartidaXadrez Partida = new PartidaXadrez();
+                while (!Partida.Terminada)
+                {
+                    Console.Clear();
+                    Tela.ImprimirTabuleiro(Partida.tab);
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
 
-                tab.ColocarPeca(new Torre (tab, Cor.Branco), new Posicao (3, 5));
-                Tela.ImprimirTabuleiro(tab);
+                    Partida.ExecutaMovimento(origem, destino);
+                }
+               
+                
+               
             }
             catch (TabuleiroExeptions ex)
             {
